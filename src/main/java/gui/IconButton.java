6c6705aa6793f3fadfdc167782c68afd2092175f
VincenzoRosarioZumbolo <vi.zumbolo@studenti.unina.it>
2025-12-21 16:53:03 +0100

    .

--- src/main/java/gui/IconButton.java
+++ src/main/java/gui/IconButton.java
@@ -14,6 +14,7 @@ public class IconButton extends JButton {
         removeListeners();
         setImageIcon(url, width, height);
         setBackground();
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
     private void removeListeners() {
