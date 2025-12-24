01e2dcabfc93c3a74709a2e71692433f282351a0
Gennaro Iacuaniello <ge.iacuaniello@studenti.unina.it>
2025-12-24 12:31:13 +0100

    Creating modules for backend and frontend

---
+++ frontend/src/main/java/frontend/dto/ProjectDTO.java
@@ -0,0 +1,75 @@
+package frontend.dto;
+
+import java.util.List;
+
+public class ProjectDTO {
+
+    private int id;
+    private String name;
+    private List<IssueDTO> issues;
+    private List<TeamDTO> teams;
+    private List<DeveloperDTO> developers;
+
+    public ProjectDTO(int id, String name, List<TeamDTO> teams, List<DeveloperDTO> developers) {
+
+        this.id = id;
+        this.name = name;
+        this.issues = null;
+        this.teams = teams;
+        this.developers = developers;
+
+    }
+
+    public ProjectDTO(int id, String name, List<IssueDTO> issues, List<TeamDTO> teams, List<DeveloperDTO> developers) {
+
+        this(id, name, teams, developers);
+
+        this.issues = issues;
+    }
+
+    public ProjectDTO(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<IssueDTO> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<IssueDTO> issues) {
+        this.issues = issues;
+    }
+
+    public List<TeamDTO> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<TeamDTO> teams) {
+        this.teams = teams;
+    }
+
+    public List<DeveloperDTO> getDevelopers() {
+        return developers;
+    }
+
+    public void setDevelopers(List<DeveloperDTO> developers) {
+        this.developers = developers;
+    }
+}
---
+++ frontend/src/main/java/frontend/dto/TeamDTO.java
@@ -0,0 +1,42 @@
+package frontend.dto;
+
+import java.util.List;
+
+public class TeamDTO {
+
+    private String name;
+    private ProjectDTO project;
+    private List<DeveloperDTO> developers;
+
+    public TeamDTO(String name, ProjectDTO project, List<DeveloperDTO> developers) {
+
+        this.name = name;
+        this.project = project;
+        this.developers = developers;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProjectDTO getProject() {
+        return project;
+    }
+
+    public void setProject(ProjectDTO project) {
+        this.project = project;
+    }
+
+    public List<DeveloperDTO> getDevelopers() {
+        return developers;
+    }
+
+    public void setDevelopers(List<DeveloperDTO> developers) {
+        this.developers = developers;
+    }
+}
